1d69e746fd43ca1f2c4c06f2ae058fad86028cbf
garlicgarrison <60302232+garlicgarrison@users.noreply.github.com>
2021-03-19 12:45:46 -0700

    store view done

--- components/cards/StoreCard.tsx
+++ components/cards/StoreCard.tsx
@@ -8,15 +8,21 @@ export default function StoreCard({store = null})
     const [imageurl, setimageurl] = useState<string>("")
     const storage = firebase.storage();
 
+    console.log(store.image_url)
+
     useEffect(() => {
-        storage.ref(store.image_url).getDownloadURL().then(url => {
+        storage.ref(`stores/${store.image_url}`).getDownloadURL().then(url => {
             setimageurl(url)
         })
     }, [])
     return (
         <div className={styles.store_card_container}>
             <img src = {imageurl} className = {styles.store_image}/>
-            {store.name}
+
+            <div className = {styles.mid_container}>
+                <h2>{store.name}</h2>
+                <span>{store.address.address1}, {store.address.city}</span>
+            </div>
         </div>
     )
 }
