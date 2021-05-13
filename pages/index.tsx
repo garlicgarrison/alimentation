2d96cb855532b851b15eb2c0d6a03850f95ea10c
garlicgarrison <60302232+garlicgarrison@users.noreply.github.com>
2021-05-12 23:59:22 -0700

    added loading svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [coordinates, setCoordinates] = useState<Object>({ latitude: null, longitude: null });
   const [locationError, setLocationError] = useState(null)
   const [address, setAddress] = useState("")
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
 
@@ -23,19 +24,25 @@ export default function Home() {
     maximumAge: 0
   }
 
-  
+
 
   const getLocation = async e => {
-    console.log("config", process.env.GEO_KEY)
-    await navigator.geolocation.getCurrentPosition(async position => {
-      if (position && position.coords)
-      {
-        let res = 
+    e.preventDefault();
+    setLoading(true)
+    navigator.geolocation.getCurrentPosition(async position => {
+      console.log("loading", loading)
+      if (position && position.coords) {
+        let res =
           await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${process.env.GEO_KEY}`)
         console.log(res)
         let addressRes = await res.json();
         console.log(addressRes)
-        setAddress(addressRes.results[0].formatted_address)
+        if (addressRes.results.length)
+        {
+          setAddress(addressRes.results[0].formatted_address)
+        }
+        setLoading(false)
+        setLocationError("Could not find address foo")
       }
     })
   }
@@ -64,13 +71,23 @@ export default function Home() {
             }
 
             <div className={styles.input_area}>
-              <input className={styles.address_input} placeholder="Address" value = {address}/>
-              <button className={styles.nav_button} onClick={getLocation}>
-                <svg width="24px" height="24px" viewBox="0 0 24 24" aria-hidden="true" fill="#8CB401">
-                  <path d="M.545 11.348l19.821-8.37a.5.5 0 0 1 .655.656l-8.369 19.82a.5.5 0 0 1-.952-.104l-1.688-9.282a.1.1 0 0 0-.08-.08L.65 12.3a.5.5 0 0 1-.105-.952z">
-                  </path>
+              <input className={styles.address_input} placeholder="Address" value={address} onChange={e => setAddress(e.target.value)}/>
+              {
+                loading &&
+                <svg className={styles.svg_loading}>
+                  <circle cx="15" cy="15" r="12"> </circle>
                 </svg>
-              </button>
+
+              }
+              {
+                !loading &&
+                <button className={styles.nav_button} onClick={getLocation}>
+                  <svg width="24px" height="24px" viewBox="0 0 24 24" aria-hidden="true" fill="#8CB401">
+                    <path d="M.545 11.348l19.821-8.37a.5.5 0 0 1 .655.656l-8.369 19.82a.5.5 0 0 1-.952-.104l-1.688-9.282a.1.1 0 0 0-.08-.08L.65 12.3a.5.5 0 0 1-.105-.952z">
+                    </path>
+                  </svg>
+                </button>
+              }
             </div>
             <Link href={{ pathname: '/stores' }}>
               <a className={styles.check_all_stores}>Check all stores</a>
