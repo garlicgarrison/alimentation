0dcb1913376a314cc3e590fa4d637258c2410c62
garlicgarrison <60302232+garlicgarrison@users.noreply.github.com>
2021-05-13 19:36:47 -0700

    added distance to store cards

--- components/cards/StoreCard.tsx
+++ components/cards/StoreCard.tsx
@@ -5,9 +5,8 @@ import "@firebase/storage"
 import { useRouter } from 'next/router';
 import { Context } from '../state/ContextProvider';
 
-export default function StoreCard({storeDoc = null})
-{
-    const {authState, setAuthState} = useContext(Context)
+export default function StoreCard({ storeDoc = null }) {
+    const { authState, setAuthState } = useContext(Context)
     const store = storeDoc.data();
     const [imageurl, setimageurl] = useState<string>("")
     const storage = firebase.storage();
@@ -24,23 +23,32 @@ export default function StoreCard({storeDoc = null})
     }, [])
 
     useEffect(() => {
-        if (firebase.auth().currentUser)
-        {
+        if (firebase.auth().currentUser) {
             firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get().then(async docRef => {
                 let userAddy = docRef.data().main_address;
-                if (!userAddy)
-                {
+                if (!userAddy) {
                     return;
                 }
                 let formattedUser = encodeURIComponent(userAddy.street1 + " " + userAddy.city + " " + userAddy.state)
                 let formattedStore = encodeURIComponent(store.address.address1 + " " + store.address.city + " " + store.address.state)
-                let distRes = await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${formattedUser}&destinations=${formattedStore}&key=${process.env.GEO_KEY}
-                `)
-                let distJSON = await distRes.json();
-                if (distJSON.status === "OK")
-                {
-                    setDistance(distJSON.rows[0].elements[0].distance.text)
+                let headers = new Headers();
+                headers.append('Content-Type', 'application/json')
+                let body = {
+                    data: {
+                        user_address: formattedUser,
+                        store_address: formattedStore
+                    }
                 }
+                let res = await fetch("https://us-central1-alimentation-851c0.cloudfunctions.net/getDistance",
+                    {
+                        method: "POST",
+                        headers: headers,
+                        body: JSON.stringify(body)
+                    }
+                )
+                let resData = await res.json();
+                console.log(resData)
+                setDistance(resData.result)
             })
         }
     }, [authState.user])
@@ -50,13 +58,13 @@ export default function StoreCard({storeDoc = null})
     }
 
     return (
-        <div className={styles.store_card_container} onClick = {handleStoreClick}>
-            <img src = {imageurl} className = {styles.store_image}/>
+        <div className={styles.store_card_container} onClick={handleStoreClick}>
+            <img src={imageurl} className={styles.store_image} />
 
-            <div className = {styles.mid_container}>
+            <div className={styles.mid_container}>
                 <h2>{store.name}</h2>
                 <span>{store.address.address1}, {store.address.city}</span>
-                <span style={{display: "block"}}>{distance}</span>
+                <span className = {styles.distance_calc} style={{ display: "block" }}>{distance}</span>
             </div>
         </div>
     )
