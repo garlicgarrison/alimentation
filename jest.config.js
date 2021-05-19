b22794f01678d2298cf0a31f86646ea0ef426b81
garlicgarrison <60302232+garlicgarrison@users.noreply.github.com>
2021-05-18 23:16:33 -0700

    added firebase mock functions

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     collectCoverageFrom: [
       '**/*.{js,jsx,ts,tsx}',
       '!**/*.d.ts',
-      '!**/node_modules/**',
+      '!**/node_modules/**'
     ],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testPathIgnorePatterns: ['/node_modules/', '/.next/'],
